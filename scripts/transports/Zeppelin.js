67940c84c9688db4d2deef43ddd1a2fa2e62d185
Nick Janssen <info@nickjanssen.com>
2013-09-25 22:22:41 +0200

    zeppelin production

--- scripts/transports/Zeppelin.js
+++ scripts/transports/Zeppelin.js
@@ -7,11 +7,16 @@ actorScripts["Zeppelin"] = State.extend({
 	},
 	Enter: function(unit) {
 
-		var waypoints = worldHandler.BuildWaypointListFromUnitIds(
-			[1,2,3]
+		this.waypoints = worldHandler.BuildWaypointListFromUnitIds(
+			[1030,1031,1032,1033,1034,1035,1036,1037,1038,1039,1040,1041,1042,1043,1044,1045,1046]
+			//[1,2,3]
 		);
 
-		unit.stateMachine.ChangeState(new PatrolLine(waypoints));
+		if ( !this.waypoints.length ) return;
+
+		unit.position.copy(this.waypoints[0].pos);
+
+		unit.stateMachine.ChangeState(new Patrol(this.waypoints));
 
 	},
 	Execute: function(unit, dTime) {
@@ -26,7 +31,38 @@ actorScripts["Zeppelin"] = State.extend({
     HandleMessage: function(unit, message, data) {
 		switch(message) {
 			case "changeWaypoint":
-				console.log("Reached waypoint "+data.id);
+				var seek = false;
+				var pause = 0;
+
+				if ( data.id === 1030 ) {
+					seek = false;
+					pause = 20000;
+				}
+				if ( data.id === 1039 ) {
+					seek = false;
+					pause = 20000;
+				}
+				if ( data.id === 1038 ) {
+					unit.maxSpeed = 1.0;
+				}
+				if ( data.id === 1040 ) {
+					unit.maxSpeed = 2.0;
+				}
+
+				if ( data.id === 1046 ) {
+					unit.maxSpeed = 1.0;
+				}
+				if ( data.id === 1031 ) {
+					unit.maxSpeed = 2.0;
+				}
+
+				unit.stateMachine.ChangeState(new Patrol(this.waypoints, {
+					seek: seek,
+					pause: pause,
+					firstWaypoint: data.id
+				}));
+
+				console.log("Going to waypoint "+data.id);
 				break;
 		}
 
