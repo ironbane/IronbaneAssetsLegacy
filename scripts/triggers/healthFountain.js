3b942e8f38a95a1f23e4b0ed234c8329e38e0664
Ben Sparks <bsparks@unicon.net>
2013-10-18 23:58:25 -0700

    gave some rpg type messages for player to know when they enter the heal zone

--- scripts/triggers/healthFountain.js
+++ scripts/triggers/healthFountain.js
@@ -1,11 +1,15 @@
 // healthFountain - script for health regen "spots"
 var healing = {
     onEnter: function(unit) {
-        //unit.Say('entered healing thing');
+        if(unit.id > 0) { // players only
+            unit.socket.emit('bigMessage', {message: 'You feel warmth wash over you.'});
+        }
         unit.SetHealth(unit.health + 1);
     },
     onExit: function(unit) {
-        //unit.Say('left healing thing');
+        if(unit.id > 0) {
+            unit.socket.emit('bigMessage', {message: 'You feel the warmth fade away.'});
+        }
     },
     onTick: function(unit) {
         unit.SetHealth(unit.health + 1);
