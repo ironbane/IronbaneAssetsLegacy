2cc44752fa2e915476a6067cfeadbe4a0e1841df
Ironbot <nikke@ironbane.com>
2013-10-31 22:12:26 +0100

    dev.ironbane.com automatic changes commit

--- gamecontent.sql
+++ gamecontent.sql
@@ -530,7 +530,7 @@ CREATE TABLE `ib_units` (
   `param` int(10) NOT NULL DEFAULT '0',
   `data` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2110 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2112 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `ib_units` WRITE;
@@ -1584,6 +1584,8 @@ INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`
 INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2107,18.62,3.00,-38.97,35,9,NULL,3,'{\"text\":\"After you take an item | you will be teleported out | of the Haunted Mansion. | | You can come back for | more rewards, however!\",\"fontSize\":20,\"rotY\":2.395814692854116}');
 INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2108,1.17,3.00,-34.30,26,30,0.13,0,'null');
 INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2109,4.37,3.00,-32.56,26,30,0.17,0,'null');
+INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2110,1.51,3.00,-49.50,27,7,NULL,0,'{\"targetExit\":2068,\"invisible\":true}');
+INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2111,1.31,3.00,-19.25,27,7,NULL,0,'{\"targetExit\":1891,\"invisible\":true}');
 /*!40000 ALTER TABLE `ib_units` ENABLE KEYS */;
 UNLOCK TABLES;
 DROP TABLE IF EXISTS `ib_zones`;
