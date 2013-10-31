5763097267049869a6f3f83c8851eff6a6f47c70
Ironbot <nikke@ironbane.com>
2013-10-31 17:22:24 +0100

    dev.ironbane.com automatic changes commit

--- gamecontent.sql
+++ gamecontent.sql
@@ -522,7 +522,7 @@ CREATE TABLE `ib_units` (
   `param` int(10) NOT NULL DEFAULT '0',
   `data` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2040 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2044 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `ib_units` WRITE;
@@ -1540,6 +1540,7 @@ INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`
 INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2037,-7.85,3.00,-24.77,32,30,4.85,0,'null');
 INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2038,-13.94,3.00,-34.39,27,7,NULL,0,'{\"targetExit\":1938,\"invisible\":true}');
 INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2039,27.46,3.00,-37.34,35,10,NULL,13,'{\"loot\":\"\",\"respawnTime\":300,\"rotY\":1.5736293855876027}');
+INSERT INTO `ib_units` (`id`, `x`, `y`, `z`, `zone`, `template`, `roty`, `param`, `data`) VALUES (2043,1.37,8.91,-26.50,28,4,NULL,40,'{\"movementType\":2,\"speedMultiplier\":1.3,\"distanceMultiplier\":2,\"rotY\":4.705185307142442}');
 /*!40000 ALTER TABLE `ib_units` ENABLE KEYS */;
 UNLOCK TABLES;
 DROP TABLE IF EXISTS `ib_zones`;
