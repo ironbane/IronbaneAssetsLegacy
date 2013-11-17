61cae99827afec9c83872fd8c55febdb9c8d5a92
Ben Sparks <bsparks@unicon.net>
2013-11-17 14:20:03 -0700

    update to prevent crashes on poison fountain too. allow NPCs to be affected as well.

--- scripts/triggers/healthFountain.js
+++ scripts/triggers/healthFountain.js
@@ -1,34 +1,46 @@
 var healing = {
     onEnter: function(unit) {
-        if(unit.isPlayer()) { // players only
-            unit.socket.emit('bigMessage', {message: 'You feel warmth wash over you.'});
-	    unit.SetHealth(unit.health + 1);
+        if (unit.isA('Fighter')) { // players only
+            if(unit.socket) {
+                unit.socket.emit('bigMessage', {
+                    message: 'You feel warmth wash over you.'
+                });
+            }
+            unit.SetHealth(unit.health + 1);
         }
     },
     onExit: function(unit) {
-	if(unit.isPlayer()) {
-		if(unit.id > 0) {
-		    unit.socket.emit('bigMessage', {message: 'You feel the warmth fade away.'});
-		}
-	}
+        if (unit.isA('Fighter')) {
+            if (unit.socket) {
+                unit.socket.emit('bigMessage', {
+                    message: 'You feel the warmth fade away.'
+                });
+            }
+        }
     },
     onTick: function(unit) {
-	if(unit.isPlayer()) {
-	   unit.SetHealth(unit.health + 1);
-	}
+        if (unit.isA('Fighter')) {
+            unit.SetHealth(unit.health + 1);
+        }
     }
 };
 
 var poison = {
     onEnter: function(unit) {
-        unit.Say("Ouch! That's poison!");
-        unit.SetHealth(unit.health - 1);
+        if (unit.isA('Fighter')) {
+            unit.Say("Ouch! That's poison!");
+            unit.SetHealth(unit.health - 1);
+        }
     },
     onExit: function(unit) {
-        unit.Say("Oh, that feels better.");
+        if (unit.isA('Fighter')) {
+            unit.Say("Oh, that feels better.");
+        }
     },
     onTick: function(unit) {
-        unit.SetHealth(unit.health - 1);
+        if (unit.isA('Fighter')) {
+            unit.SetHealth(unit.health - 1);
+        }
     }
 };
 
