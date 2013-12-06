52fad4b6f8d2152eeb3f00d6557e638ca88fa089
Ironbot <nikke@ironbane.com>
2013-12-06 05:10:05 -0700

    dev.ironbane.com automatic changes commit

--- gamecontent.sql
+++ gamecontent.sql
@@ -603,7 +603,7 @@ CREATE TABLE `ib_units` (
   `param` int(10) NOT NULL DEFAULT '0',
   `data` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3092 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3102 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `ib_units` WRITE;
